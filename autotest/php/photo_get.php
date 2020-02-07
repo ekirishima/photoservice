ce1bf92da11969f9029c7641ee245a19015d737a
ekirishima <russselo@gmail.com>
2020-02-07 20:13:49 +0500

    photo get check

---
+++ autotest/php/photo_get.php
@@ -0,0 +1,26 @@
+<?php 
+
+    try {
+
+        $url = $_REQUEST["url"];
+        header("Content-type: application/json; charset=utf-8");
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url."/api/photo");
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '.$_REQUEST["header"])); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $header = substr($data, 0, $header_size);
+        $body = substr($data, $header_size);
+
+        echo json_encode(array("code" => $httpCode, "response" => json_decode($body)));
+
+        curl_close($ch);
+
+    } catch (\Throwable $th) {
+        throw $th;
+    }
