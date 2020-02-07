dd337c81e6f246f146a1d97af9b345ca31cfcc57
ekirishima <russselo@gmail.com>
2020-02-07 20:16:56 +0500

    docs add

--- autotest/php/login.php
+++ autotest/php/login.php
@@ -6,15 +6,20 @@
         header("Content-type: application/json; charset=utf-8");
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url."/api/logout");
+        curl_setopt($ch, CURLOPT_URL, $url."/api/login");
         curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '.$_REQUEST["header"])); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $string);
         $data = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        
-        echo json_encode(array("code" => $httpCode));
+
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $header = substr($data, 0, $header_size);
+        $body = substr($data, $header_size);
+
+        echo json_encode(array("code" => $httpCode, "response" => json_decode($body)));
 
         curl_close($ch);
 
